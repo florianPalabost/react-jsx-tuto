a46e9749d5dda45a92ee0bf63eba819cab710aa8
Florian <palabostg@gmail.com>
2023-11-14 11:31:06 +0100

    init react project

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    endOfLine: 'lf',
+    semi: true,
+    bracketSpacing: true,
+    // bracketSameLine: false,
+    trailingComma: 'none',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4
+};

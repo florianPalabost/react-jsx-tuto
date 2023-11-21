d0e7537cdcd8b03d8f59b3ab2df0a1e688c8a258
Florian <palabostg@gmail.com>
2023-11-21 18:19:20 +0100

    init useMemo

---
+++ src/components/Hooks/UseMemo/UseMemo.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
+import MyInput from '../../Forms/MyInput';
+
+export default function UseMemo() {
+    return (
+        <Box>
+            <h1></h1>
+        </Box>
+    );
+}
